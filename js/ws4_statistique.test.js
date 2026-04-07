efc0eb0b0ed8d2686806172e2c806f0a05e8bc38
Elia <elia.soinard@outlook.fr>
2026-04-07 08:55:56 +0200

    ajout de test

---
+++ js/ws4_statistique.test.js
@@ -0,0 +1,22 @@
+/**
+ * @jest-environment jsdom
+ */
+
+require('jest');
+
+/*pour chaque cas qu'on veut tester*/
+test('Decrire ce que sa test', () =>{
+
+
+
+    /*Il faut recharger le fichier d'origine*/
+    require('lien jusqua origine ex : ../js/ws4_statistique.js');
+});
+
+
+
+
+
+
+
+
